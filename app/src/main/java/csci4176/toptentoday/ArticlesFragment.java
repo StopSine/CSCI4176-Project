12dfb46f3ae687f34d2201c8f086dda80d8b1e7f
stopsine <Blair_Elliott@live.ca>
2016-03-05 17:48:16 -0400

    commit the things

---
+++ app/src/main/java/csci4176/toptentoday/ArticlesFragment.java
@@ -0,0 +1,31 @@
+package csci4176.toptentoday;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.util.Log;
+
+public class ArticlesFragment extends ListFragment {
+
+    private static final String TAG = "MoviesFrag";
+
+    String[] numbers = new String[] { "one", "two", "three", "four",
+            "five", "six", "seven", "eight", "nine", "ten", "eleven",
+            "twelve", "thirteen", "fourteen", "fifteen" };
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(), R.layout.list_item, R.id.mainText, numbers);
+        setListAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Log.d(TAG, "id=" + id);
+    }
+}
---
+++ app/src/main/java/csci4176/toptentoday/PagerAdapter.java
@@ -0,0 +1,37 @@
+package csci4176.toptentoday;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                ArticlesFragment articles = new ArticlesFragment();
+                return articles;
+            case 1:
+                MoviesFragment movies = new MoviesFragment();
+                return movies;
+            case 2:
+                ShowsFragment shows = new ShowsFragment();
+                return shows;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
